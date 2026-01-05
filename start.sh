84b7104649eb9998af59efaf038d33d11c3ae409
SeavminhICT <seavminh22220@gmail.com>
2026-01-05 12:19:41 +0700

    Increase gunicorn timeout

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ cd khmer25_api_django/crm
 mkdir -p staticfiles
 mkdir -p "${MEDIA_ROOT:-media}"
 python manage.py collectstatic --noinput
-gunicorn crm.wsgi:application --bind 0.0.0.0:${PORT}
+: "${GUNICORN_TIMEOUT:=120}"
+gunicorn crm.wsgi:application --bind 0.0.0.0:${PORT} --timeout "${GUNICORN_TIMEOUT}"
